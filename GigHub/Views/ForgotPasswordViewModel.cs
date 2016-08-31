5e5532ad6773c8d6ffa7a57bc3d576061c204456
carlos <cacalder@cisco.com>
2016-08-31 01:53:59 -0500

    add name to artist

---
+++ GigHub/Views/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Views
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
